c7e680033bd2f13dacd7e8469177b08de3fba339
joeyjumper94 <joeyjumper94@gmail.com>
2018-02-04 15:19:12 -0800

    new check
    added a check to make sure chat commands affect only one player

--- lua/ppm/chatcommands.lua
+++ lua/ppm/chatcommands.lua
@@ -1,5 +1,6 @@
 if CLIENT then
 	hook.Add("OnPlayerChat","ppm_chat_commands",function(ply,text,Team,Dead)
+		if ply!=LocalPlayer() then return end--this will make it so that only the player who said something will have a command run
 		text=string.lower(text)
 		if string.StartWith(text,"!ppm") or string.StartWith(text,"/ppm") then
 			cmd=string.Split(string.Split(text,"ppm")[2]," ")
