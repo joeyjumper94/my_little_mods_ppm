d8e67a03d82084f31db1e617a2160d4eaf5a9f93
joeyjumper94 <joeyjumper94@gmail.com>
2018-02-04 12:32:21 -0800

    built in fastDL
    controlled by ppm_fastdl, set this to 0 to disable
    restart after changing

--- lua/autorun/ppm.lua
+++ lua/autorun/ppm.lua
@@ -2,7 +2,7 @@ AddCSLuaFile()
 print("Loading Equestrian Wastelanders PPM")
 player_manager.AddValidModel("pony","models/ppm/player_default_base.mdl")
 player_manager.AddValidModel("ponynj","models/ppm/player_default_base_nj.mdl")   
- 
+local FastDL=CreateConVar("ppm_Fastdl","1",FCVAR_ARCHIVE,FCVAR_REPLICATED,FCVAR_SERVER_CAN_EXECUTE,"should we automatically add this to the list of stuff people download while joining?"):GetBool()
 if SERVER then
 	local function add_files(dir)
 		local files,folders = file.Find(dir.."*", "LUA")
@@ -15,7 +15,9 @@ if SERVER then
 			add_files(dir..folder_name.."/")
 		end
 	end
-
+	if FastDL then
+		resource.AddWorkshop("945735699")
+	end
 	add_files("ppm/")
 else
 	list.Set("PlayerOptionsModel","pony","models/ppm/player_default_base.mdl") 
