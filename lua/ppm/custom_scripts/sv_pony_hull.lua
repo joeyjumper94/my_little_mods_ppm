7ed4ac3c5e5fde481712fee9a6d6fe826190f7e9
joeyjumper94 <joeyjumper94@gmail.com>
2024-06-23 04:10:14 -0700

    delay player model check by 1 frame so that it works right

--- lua/ppm/custom_scripts/sv_pony_hull.lua
+++ lua/ppm/custom_scripts/sv_pony_hull.lua
@@ -1,17 +1,21 @@
 local name="ppm_pony_hull"
 local ConVar=CreateConVar(name,"1",bit.bor(FCVAR_ARCHIVE,FCVAR_REPLICATED),"enable modified hull sizes for ponies",0,1)
 PPM.SetHull=function(Player,localvals)
-	if Player:IsPlayer()and PPM.isValidPonyLight(Player)and ConVar:GetBool()then
-		Player[name]=true
-		local ponydata=PPM.getPonyValues(Player,localvals)
-		local bodyheight=math.Clamp(ponydata.bodyheight,PPM.height_min,PPM.height_max)*.05
-		local neckheight=math.Clamp(ponydata.neckheight,PPM.height_min,PPM.height_max)*.05
-		local scale=0.9+bodyheight+neckheight
-		Player:SetHull(Vector(-16,-16,0),Vector(16,16,55*scale))
-	elseif Player[name]then
-		Player:ResetHull()
-		Player[name]=nil
-	end
+	timer.Simple(0,function()
+		if Player:isValid()then
+			if Player:IsPlayer()and PPM.isValidPonyLight(Player)and ConVar:GetBool()then
+				Player[name]=true
+				local ponydata=PPM.getPonyValues(Player,localvals)
+				local bodyheight=math.Clamp(ponydata.bodyheight,PPM.height_min,PPM.height_max)*.05
+				local neckheight=math.Clamp(ponydata.neckheight,PPM.height_min,PPM.height_max)*.05
+				local scale=0.9+bodyheight+neckheight
+				Player:SetHull(Vector(-16,-16,0),Vector(16,16,55*scale))
+			elseif Player[name]then
+				Player:ResetHull()
+				Player[name]=nil
+			end
+		end
+	end)
 end
 hook.Add("PlayerLoadout",name,PPM.SetHull)
 hook.Add("PlayerSetModel",name,PPM.SetHull)
